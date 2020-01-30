1385229deef0c171856142e5532193a0ac089a5b
Shrikant Vishwakarma <zeeroiq@gmail.com>
2020-01-31 00:20:33 +0530

    added spring-boot application and test source

---
+++ src/main/java/com/shri/sms/SpringBootSmsApplication.java
@@ -0,0 +1,13 @@
+package com.shri.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSmsApplication.class, args);
+    }
+
+}
